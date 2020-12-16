30dd5663959fafd8ecd2854379c3efdcd278139c
Марина Редька <marinaredka1@gmail.com>
2020-12-16 18:12:51 +0200

    Network

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+
+public class Player : NetworkBehaviour
+{
+    float DeltaMovement = 0.03f;
+    public const int MaxHealth = 100;
+    [SyncVar] int Health = MaxHealth;
+    public Text HealthBar;
+    private void Update()
+    {
+        HandleMovement();
+        HealthBar.text = System.Convert.ToString(Health);
+
+        void HandleMovement()
+        {
+            if (isLocalPlayer)
+            {
+                float MoveHorizontal = Input.GetAxis("Horizontal");
+                float MoveVertical = Input.GetAxis("Vertical");
+                Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+                transform.position = transform.position + movement * DeltaMovement;
+            }
+        }
+    }
+}
